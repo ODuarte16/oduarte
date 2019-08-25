98cb028d41e4da6eb047b1c54c8d90f7c382dee5
ODuarte16 <54487468+ODuarte16@users.noreply.github.com>
2019-08-25 16:04:12 -0700

    uploaded solution

---
+++ prob2.py
@@ -0,0 +1,25 @@
+import random
+import string
+import hashlib
+
+
+def randomString(stringLength=10):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+   
+   
+
+def solution(my_string):
+    for i in range(len(my_string)):
+        j = i + 1
+        while j < len(my_string):
+            if my_string[i] == my_string[j]:
+                return my_string[i]
+            j += 1  
+    return "Null"            
+
+
+if __name__ == '__main__':
+    my_string = randomString() #string to fuck with
+    print(my_string)
+    print(solution(my_string))
